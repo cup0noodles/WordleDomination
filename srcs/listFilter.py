ff353f4018ba69f8ed72818d52afe75206b18bd3
mkw_pop <mattkadenwong@gmail.com>
2022-01-25 00:43:11 -0800

    added global/local black/whitelist functions

--- srcs/listFilter.py
+++ srcs/listFilter.py
@@ -14,7 +14,7 @@ class Words:
     Class for words, calculating score etc.
     """
 
-    def __init__(self, word, pos_score, abs_score):
+    def __init__(self, word, pos_score=0, abs_score=0):
         self.word = word
         self.pos_score = pos_score
         self.abs_score = abs_score
@@ -37,18 +37,64 @@ class LetterStats:
         self.pos5 = pos5
 
 
-def filter_blacklist():
+def convert_array_class(word_array):
+    """
+
+    :param word_array:
+    """
+    for word in word_array:
+        filtered_class.append(Words(word))
+
+
+def filter_blacklist(word_class, letter, position):
+    filtered_word_class = []
+    for i in word_class:
+        letter_check = i.return_letter(position)
+        if letter_check != letter:
+            filtered_word_class.append(i)
+    return filtered_word_class
+
+
+def filter_global_blacklist(word_class, letter):
+    filtered_word_class = []
+    for i in word_class:
+        for j in range(1, 6):
+            letter_check = i.return_letter(j)
+            if letter_check == letter:
+                break
+            elif j == 5:
+                filtered_word_class.append(i)
+            else:
+                continue
+
+    return filtered_word_class
+
+
+def filter_whitelist(word_class, letter, position):
     """
 
     :return:
     """
+    filtered_word_class = []
+    for i in word_class:
+        letter_check = i.return_letter(position)
+        if letter_check == letter:
+            filtered_word_class.append(i)
+    return filtered_word_class
 
 
-def filter_whitelist():
-    """
+def filter_global_whitelist(word_class, letter):
+    filtered_word_class = []
+    for i in word_class:
+        for j in range(1, 6):
+            letter_check = i.return_letter(j)
+            if letter_check == letter:
+                filtered_word_class.append(i)
+                break
+            else:
+                continue
 
-    :return:
-    """
+    return filtered_word_class
 
 
 def score_array():
@@ -110,7 +156,49 @@ if __name__ == '__main__':
     # words = read_input("dict.txt")
     # filtered = filter_length(words, 5)
     filtered = read_input("filtered.txt", True)
-    # print(len(filtered))
+    filtered_class = []
+    convert_array_class(filtered)
+    """
+    Solution for 1/24 - 3 tries
+    # From Guess 1
+    filtered_class = filter_global_blacklist(filtered_class, 'a')
+    filtered_class = filter_global_blacklist(filtered_class, 't')
+    filtered_class = filter_global_blacklist(filtered_class, 'e')
+    filtered_class = filter_whitelist(filtered_class, 'o', 3)
+    filtered_class = filter_blacklist(filtered_class, 'n', 4)
+    # From Guess 2
+    filtered_class = filter_global_blacklist(filtered_class, 's')
+    filtered_class = filter_global_blacklist(filtered_class, 'c')
+    filtered_class = filter_global_blacklist(filtered_class, 'r')
+    filtered_class = filter_blacklist(filtered_class, 'n', 5)
+    filtered_class = filter_global_whitelist(filtered_class, 'n')
+    """
+
+    """
+    Solution for 1/25 - 4 tries
+    filtered_class = filter_global_whitelist(filtered_class, 'a')
+    filtered_class = filter_global_whitelist(filtered_class, 's')
+    filtered_class = filter_global_blacklist(filtered_class, 't')
+    filtered_class = filter_global_blacklist(filtered_class, 'o')
+    filtered_class = filter_global_blacklist(filtered_class, 'n')
+    filtered_class = filter_global_blacklist(filtered_class, 'e')
+    filtered_class = filter_blacklist(filtered_class, 'a', 1)
+    filtered_class = filter_blacklist(filtered_class, 'a', 2)
+    filtered_class = filter_blacklist(filtered_class, 's', 3)
+    filtered_class = filter_global_blacklist(filtered_class, 'd')
+    filtered_class = filter_global_blacklist(filtered_class, 'c')
+    filtered_class = filter_global_blacklist(filtered_class, 'h')
+    filtered_class = filter_global_blacklist(filtered_class, 'i')
+    filtered_class = filter_whitelist(filtered_class, 's', 1)
+    filtered_class = filter_whitelist(filtered_class, 'r', 5)
+    filtered_class = filter_whitelist(filtered_class, 'a', 4)
+    filtered_class = filter_global_whitelist(filtered_class, 'u')
+    filtered_class = filter_blacklist(filtered_class, 'u', 4)
+    """
+    for word in filtered_class:
+        print(word.word)
+
+    print(len(filtered_class))
     # scores = score_array()
     # print(scores)
     # score_dict = {}
